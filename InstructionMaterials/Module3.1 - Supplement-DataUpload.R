c3c62e035720bda45394086f7855ac5035154c8a
Amandine Gamble <amandine.gamble@gmail.com>
2022-02-21 21:48:01 -0500

    progressed on 3.1

--- InstructionMaterials/Module3.1 - Supplement-DataUpload.R
+++ InstructionMaterials/Module3.1 - Supplement-DataUpload.R
@@ -10,27 +10,55 @@ for(i in c("counts_raw.csv", "counts_transformed.csv", "sample_info.csv", "test_
   )
 }
 
-raw_cts = read.csv("Data/RNAData/counts_raw.csv")
+# raw_cts = read.csv("Data/RNAData/counts_raw.csv")
 trans_cts = read.csv("Data/RNAData/counts_transformed.csv")
 sample_info = read.csv("Data/RNAData/sample_info.csv")
 test_result = read.csv("Data/RNAData/test_result.csv")
 
-# load the package
-library(tidyverse)
 
-# "gather" the counts data
-trans_cts_long <- trans_cts %>% 
-  pivot_longer(cols = wt_0_r1:mut_180_r3, 
-               names_to = "sample", 
-               values_to = "cts")
+# Create a matrix from our table of counts
+pca_matrix <- trans_cts %>% 
+  # make the "gene" column become the rownames of the table
+  column_to_rownames("gene") %>% 
+  # coerce to a matrix
+  as.matrix() %>% 
+  # transpose the matrix so that rows = samples and columns = variables
+  t()
 
-# raw_cts_long <- raw_cts %>% 
-#   pivot_longer(cols = wt_0_r1:mut_180_r3, 
-#                names_to = "sample", 
-#                values_to = "cts")
 
-trans_cts_long <- full_join(trans_cts_long, sample_info, by = "sample")
+# or get the data and plot it yourself
+pca_scale <- prcomp(pca_matrix, scale = TRUE)
 
-#raw_cts_long <- full_join(raw_cts_long, sample_info, by = "sample")
 
-write.csv(trans_cts_long, file = "Data/RNAData/counts_transformed_tidy.csv")
+# The PC scores are stored in the "x" value of the prcomp object
+pc_scores <- as.data.frame(pca_scale$x)
+pc_scores$sample = rownames(pc_scores)
+pc_scores = pc_scores %>% relocate(sample)
+
+pc_scores_comp = full_join(pc_scores, sample_info, by = "sample")
+
+pc_loadings <- as.data.frame(pca_scale$rotation)
+pc_loadings$gene = rownames(pc_loadings)
+pc_loadings = pc_loadings %>% relocate(gene)
+
+top_genes <- pc_loadings %>% 
+  # select only the PCs we are interested in
+  select(gene, PC1, PC2) %>%
+  # convert to a "long" format
+  pivot_longer(matches("PC"), names_to = "PC", values_to = "loading") %>% 
+  # for each PC
+  group_by(PC) %>% 
+  # arrange by descending order of loading
+  arrange(desc(abs(loading))) %>% 
+  # take the 10 top rows
+  slice(1:10) %>% 
+  # pull the gene column as a vector
+  pull(gene) %>% 
+  # ensure only unique genes are retained
+  unique()
+
+top_loading = pc_loadings[is.element(pc_loadings$gene, top_genes), ]
+
+trans_cts_sub = trans_cts[is.element(trans_cts$gene, top_genes), ]
+
+write.csv(trans_cts_sub, "Data/RNAData/counts_transformed_subset.csv", row.names = F)
