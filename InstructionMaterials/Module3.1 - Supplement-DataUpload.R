3e53132b2cc65214f8fd2033e7e6119e2f00b602
Amandine Gamble <amandine.gamble@gmail.com>
2022-02-22 16:02:26 -0500

    Cleaned supplement to 3.1

--- InstructionMaterials/Module3.1 - Supplement-DataUpload.R
+++ InstructionMaterials/Module3.1 - Supplement-DataUpload.R
@@ -1,8 +1,10 @@
+# Data upload and import #######################################################################
+
 # Create a "data" directory
 dir.create("Data/RNAData")
 
-# Download the data provided by your collaborator
-# using a for loop to automate this step
+# Download the data 
+# Use a loop to automate this step
 for(i in c("counts_raw.csv", "counts_transformed.csv", "sample_info.csv", "test_result.csv")){
   download.file(
     url = paste0("https://github.com/tavareshugo/data-carpentry-rnaseq/blob/master/data/", i, "?raw=true"),
@@ -10,11 +12,17 @@ for(i in c("counts_raw.csv", "counts_transformed.csv", "sample_info.csv", "test_
   )
 }
 
+# Import the data in the environment
 # raw_cts = read.csv("Data/RNAData/counts_raw.csv")
 trans_cts = read.csv("Data/RNAData/counts_transformed.csv")
 sample_info = read.csv("Data/RNAData/sample_info.csv")
 test_result = read.csv("Data/RNAData/test_result.csv")
 
+# Data subsetting #######################################################################
+
+# To subset the data, we will run a principal component analysis
+# to identify the genes explaining most of the variance
+# This part is redundant with module 3.1, but considers a bigger data set
 
 # Create a matrix from our table of counts
 pca_matrix <- trans_cts %>% 
@@ -25,22 +33,15 @@ pca_matrix <- trans_cts %>%
   # transpose the matrix so that rows = samples and columns = variables
   t()
 
-
-# or get the data and plot it yourself
+# Run the PCA
 pca_scale <- prcomp(pca_matrix, scale = TRUE)
 
-
-# The PC scores are stored in the "x" value of the prcomp object
-pc_scores <- as.data.frame(pca_scale$x)
-pc_scores$sample = rownames(pc_scores)
-pc_scores = pc_scores %>% relocate(sample)
-
-pc_scores_comp = full_join(pc_scores, sample_info, by = "sample")
-
+# Extract the PC loadings
 pc_loadings <- as.data.frame(pca_scale$rotation)
 pc_loadings$gene = rownames(pc_loadings)
 pc_loadings = pc_loadings %>% relocate(gene)
 
+# Identify the 'top genes', explaining to most of the variance
 top_genes <- pc_loadings %>% 
   # select only the PCs we are interested in
   select(gene, PC1, PC2) %>%
@@ -57,8 +58,8 @@ top_genes <- pc_loadings %>%
   # ensure only unique genes are retained
   unique()
 
-top_loading = pc_loadings[is.element(pc_loadings$gene, top_genes), ]
-
+# Create a filtered data set, with only the top genes
 trans_cts_sub = trans_cts[is.element(trans_cts$gene, top_genes), ]
 
+# Save this data set
 write.csv(trans_cts_sub, "Data/RNAData/counts_transformed_subset.csv", row.names = F)
